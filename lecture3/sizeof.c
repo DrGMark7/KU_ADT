cf899fdbebb83f5c43dad3b9ab72a97b1d45134f
Vin <chokun5588@gmail.com>
2024-07-09 14:46:34 +0700

    Lecture 3 Note

--- lecture3/sizeof.c
+++ lecture3/sizeof.c
@@ -12,6 +12,8 @@ int main(void){
     printf("Size of double a     : %ld\n", sizeof(double));     //. 8
     printf("Size of long a       : %ld\n", sizeof(long));       //. 8
     printf("Size of long long a  : %ld\n", sizeof(long long));  //. 8
-    printf("Size of long double a: %ld\n", sizeof(long double));//. 8 but it should be 16
+    printf("Size of long double a: %ld\n", sizeof(long double));//. 8 but it should be 16 Becuase Compiler Verision
+                                                                //. if you use Mac (C17) Compiler you will get 8 Bytes
+                                                                //.  else you use C99 Compiler you will get 16 Bytes
     printf("Size of size_t       : %ld\n", sizeof(size_t));     //. 8
 }
